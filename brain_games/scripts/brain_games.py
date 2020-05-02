1d35cf2033f14fbb078e8023ae9a807b1fb02aee
Shynggys Zharas <sm.zharas@gmail.com>
2020-05-02 12:24:14 +0600

    codeclimate, flake8 and travis added

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+from brain_games.cli import welcome_user
+
 
 def greet():
     print('Welcome to the Brain Games!')
+    welcome_user()
+
 
 def main():
     greet()
 
+
 if __name__ == '__main__':
-	main()
+    main()

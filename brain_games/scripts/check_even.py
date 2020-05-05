faa557de2084113b7180c2cffb401215306ffb3f
Shynggys Zharas <sm.zharas@gmail.com>
2020-05-05 20:15:37 +0600

    while loop add to check_even

--- brain_games/scripts/check_even.py
+++ brain_games/scripts/check_even.py
@@ -4,19 +4,24 @@ from random import randint
 
 
 def start_game():
-    random_num = randint(1, 100)
-    is_even = random_num % 2 == 0
-    correct_answer = 'yes' if is_even else 'no'
+    count = 0
+    while count < 3:
+        random_num = randint(1, 100)
+        is_even = random_num % 2 == 0
+        correct_answer = 'yes' if is_even else 'no'
 
-    print('Question: {}'.format(random_num))
-    user_input = prompt.string('Your answer: ')
+        print('Question: {}'.format(random_num))
+        user_input = prompt.string('Your answer: ')
 
-    if is_even and user_input == correct_answer:
-        print('Correct!')
-    elif not is_even and user_input == correct_answer:
-        print('Correct!')
-    else:
-        print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(user_input, correct_answer))
+        if is_even and user_input == correct_answer:
+            print('Correct!')
+            count += 1
+        elif not is_even and user_input == correct_answer:
+            print('Correct!')
+            count += 1
+        else:
+            print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(
+                user_input, correct_answer))
 
 
 def main():

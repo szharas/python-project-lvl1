d892a1be17184395c8e0072e9ca67abc5091ac4c
Shynggys Zharas <sm.zharas@gmail.com>
2020-05-23 20:39:43 +0600

    naming fixed in progression game

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -6,17 +6,17 @@ PROGRESSION_LENGHT = 10  # runs 10 times
 
 
 def generate_question():
-    numbers = []
+    progression = []
     start_num = randint(1, 10)
     step = randint(1, 10)
     last_num = PROGRESSION_LENGHT * step + start_num
 
     for i in range(start_num, last_num, step):
-        numbers.append(i)
+        progression.append(i)
 
     random_choice = randint(0, PROGRESSION_LENGHT - 1)  # get index from 0 to 9
-    correct_answer = str(numbers[random_choice])
-    numbers[random_choice] = '..'
-    question = ' '.join(str(e) for e in numbers)
+    correct_answer = str(progression[random_choice])
+    progression[random_choice] = '..'
+    question = ' '.join(str(e) for e in progression)
 
     return question, correct_answer

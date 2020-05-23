f974a011d57cc9a0af9e33df82293192a784d5c5
Shynggys Zharas <sm.zharas@gmail.com>
2020-05-23 19:21:51 +0600

    magic num and naming changed

--- brain_games/games/game_engine.py
+++ brain_games/games/game_engine.py
@@ -2,22 +2,23 @@ import prompt
 
 
 def play_game(game, user_name):
+    NO_OF_TRIES = 3
     count = 0
 
-    while count < 3:
+    while count < NO_OF_TRIES:
         question, correct_answer = game.generate_question()
 
         print('Question: {}'.format(question))
-        user_input = prompt.string('Your answer: ')
+        answer = prompt.string('Your answer: ')
 
-        if user_input == correct_answer:
+        if answer == correct_answer:
             print('Correct!')
             count += 1
-        elif user_input == 'exit':
+        elif answer == 'exit':
             break
         else:
             print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(
-                user_input, correct_answer))
+                answer, correct_answer))
             print("Let's try again, {}!".format(user_name))
 
     print("Congratulations, {}!".format(user_name))

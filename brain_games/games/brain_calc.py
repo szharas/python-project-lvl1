ef9afce8097498b0dc83c6d31dd0bfe83347c0fe
Shynggys Zharas <sm.zharas@gmail.com>
2020-05-23 19:27:55 +0600

    brain_calc optimized

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -7,20 +7,18 @@ DESCRIPTION = 'What is the result of the expression?'
 def generate_question():
     question = ''
     correct_answer = ''
-    operators = ['add', 'substract', 'multiply']
 
     operand_1 = randint(1, 100)
     operand_2 = randint(1, 100)
-    operator = choice(operators)
+    operator = choice('-+*')
 
-    if operator == 'add':
-        question = '{} + {}'.format(operand_1, operand_2)
+    question = '{} {} {}'.format(operand_1, operator, operand_2)
+
+    if operator == '+':
         correct_answer = str(operand_1 + operand_2)
-    elif operator == 'substract':
-        question = '{} - {}'.format(operand_1, operand_2)
+    elif operator == '-':
         correct_answer = str(operand_1 - operand_2)
-    elif operator == 'multiply':
-        question = '{} * {}'.format(operand_1, operand_2)
+    elif operator == '*':
         correct_answer = str(operand_1 * operand_2)
 
     return question, correct_answer

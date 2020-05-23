7f82450063b88294aa8e77595a886ac8071a066b
Shynggys Zharas <sm.zharas@gmail.com>
2020-05-23 20:18:54 +0600

    brain_progression naming optimized

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -2,19 +2,19 @@ from random import randint
 
 
 DESCRIPTION = 'What number is missing in the progression?'
-ITERATION = 10  # runs 10 times
+PROGRESSION_LENGHT = 10  # runs 10 times
 
 
 def generate_question():
     numbers = []
     start_num = randint(1, 10)
     step = randint(1, 10)
-    last_num = ITERATION * step + start_num
+    last_num = PROGRESSION_LENGHT * step + start_num
 
     for i in range(start_num, last_num, step):
         numbers.append(i)
 
-    random_choice = randint(0, ITERATION - 1)  # get index from 0 to 9
+    random_choice = randint(0, PROGRESSION_LENGHT - 1)  # get index from 0 to 9
     correct_answer = str(numbers[random_choice])
     numbers[random_choice] = '..'
     question = ' '.join(str(e) for e in numbers)
